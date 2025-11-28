99e15b0f03f46393938d4e6203e890270396baa7
valli25 <pravallika.tsm@gmail.com>
2025-11-28 05:55:24 -0800

    adding files

--- 02_strings.py
+++ 02_strings.py
@@ -4,3 +4,23 @@ print (sample_str)
 # How to take individual character from a string
 
 print (sample_str[8])
+
+# slicing
+
+sub_str = sample_str[2:7]
+print(sub_str)
+
+sub_str = sample_str[:]
+print(sub_str)
+
+sub_str = sample_str[1:]
+print(sub_str)
+
+sub_str = sample_str[:5]
+print(sub_str)
+
+sub_str = sample_str[::2]
+print(sub_str)
+
+sub_str = sample_str[::-1]
+print(sub_str)
