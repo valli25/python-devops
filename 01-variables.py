80fe2e5b4547c10d64278d276bd7e9bcff1ab445
valli25 <pravallika.tsm@gmail.com>
2025-11-27 16:31:23 -0800

    adding files

--- 01-variables.py
+++ 01-variables.py
@@ -1,2 +1,28 @@
 a = 42
 print(a)
+
+b = 42.45
+print(b)
+
+c = True
+
+#strings
+d = 'this is a string'
+e = "this is also a string"
+f = """
+  this is a multi line string 
+  """
+
+d = 'Today\'s weather is nice'
+print(d)
+
+d = "Today' s weather is nice"
+print(d)
+
+
+"""
+- List
+- Tuple
+- Set
+- Dictionary
+"""
