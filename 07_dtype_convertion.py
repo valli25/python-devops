4c679a1356712af1f6b689c381f3e29b37e8a030
valli25 <pravallika.tsm@gmail.com>
2025-11-28 16:54:52 -0800

    adding files

--- 07_dtype_convertion.py
+++ 07_dtype_convertion.py
@@ -1,3 +1,14 @@
 sample_str = "this is a sample string"
 sample_str_list = list(sample_str)
 print(sample_str_list)
+
+sample_str_tuple = tuple(sample_str)
+print(sample_str_tuple)
+
+# accept input from a user
+
+user_input = input("enter a number:")
+print(user_input, type(user_input))
+
+add_10 = int(user_input) + 10
+print(add_10)
