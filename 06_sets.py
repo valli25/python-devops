89a1739ebc76cc8d3b38f70b45218c00e07ace4c
valli25 <pravallika.tsm@gmail.com>
2025-11-28 16:48:40 -0800

    adding files

---
+++ 06_sets.py
@@ -0,0 +1,12 @@
+sample_set = {1,2,3,5,7,3}
+print(sample_set)
+
+# set returns unique element that is stored 
+# inside that variable
+#set don't consider the order of insertion
+#sets do not support indexing
+#add elements to a set
+sample_set = {1,2,6,4,5,4}
+sample_set.add(6)
+print(sample_set)
+
