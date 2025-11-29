22992ca1b33988f79fb97a45f435c308b71bde1b
valli25 <pravallika.tsm@gmail.com>
2025-11-28 16:02:03 -0800

    adding files

---
+++ 05_dict.py
@@ -0,0 +1,8 @@
+sample_dict = {1: 1, 2: 4, 3: 9} #dict()
+#key in dictionary should of immutable datatype
+
+print(sample_dict[3])
+
+sample_dict = {1: 1, 2: 4, 3: 9, 3: 15}
+print(sample_dict[3])
+
